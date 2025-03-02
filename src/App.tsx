014f4f655488cd7e4758ce6ce88b75ce75af116a
马兆铿 <810768333@qq.com>
2025-03-02 17:38:11 +0800

    🏗️: Change ui lib to Arco-react

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,11 @@ import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import './App.scss'
 import { router } from './router'
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 
 function App() {
   return (
     <div className="App">
-      <ChakraProvider value={defaultSystem}>
-        <RouterProvider router={router} />
-      </ChakraProvider>
+      <RouterProvider router={router} />
     </div>
   )
 }
--- src/pages/test-redux/index.tsx
+++ src/pages/test-redux/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import { Button, HStack } from '@chakra-ui/react'
 
 import { Page } from '@/components'
+import { Button } from '@arco-design/web-react'
 
 const Index: React.FC = () => {
   return (
     <Page title="组件通信测试" className="justify-center items-center">
-      <HStack>
-        <Button className="mb-120">Click me</Button>
-        <Button>Click me</Button>
-      </HStack>
+      <Button type="primary">Hello Arco</Button>
     </Page>
   )
 }
