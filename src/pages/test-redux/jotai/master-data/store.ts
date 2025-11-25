99dead41f13e369ebdc54e2594ae08a6a1c62151
马兆铿 <810768333@qq.com>
2025-11-25 09:31:07 +0800

    🏗️: Standardize all page names

--- src/pages/test-redux/jotai/master-data/store.ts
+++
@@ -1,4 +0,0 @@
-import { atom } from 'jotai'
-import { MasterDataId } from '@/pages/test-redux/utils'
-
-export const tempAtom = atom<Record<MasterDataId, Obj>>({})
--- src/pages/test-redux/proxy/master-data/store.ts
+++
@@ -1,5 +0,0 @@
-import { MasterDataId } from '@/pages/test-redux/utils'
-import { createObservable } from '@/packages/observer'
-
-const temp: Partial<Record<MasterDataId, any>> = {}
-export const tempObservable = createObservable(temp)
